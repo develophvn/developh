24dc96d463479638ffad2d47a566702a62a1635a
Dat Do <dat.b.do@gmail.com>
2021-02-24 22:20:01 -0500

    fix: update about info

--- packages/static-ui/src/containers/Home/index.js
+++ packages/static-ui/src/containers/Home/index.js
@@ -2,19 +2,17 @@ import * as React from "react";
 import { styled } from "baseui";
 import Landing from "./landing";
 import OfferInfo from "./offerinfo";
-import AboutInfo from "./aboutinfo";
 
 const Centered = styled("div", {
   textAlign: "center",
 });
 
 // markup
-const Home = ({ offers, aboutInfo }) => {
+const Home = ({ offers }) => {
   return (
     <Centered>
       <Landing />
       <OfferInfo offers={offers} />
-      <AboutInfo aboutInfo={aboutInfo} />
     </Centered>
   );
 };
